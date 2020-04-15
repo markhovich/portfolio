0665eaec6809b96c69ad2e7077f31c230c14ff10
julienm <marque_julien@hotmail.fr>
2020-04-15 10:28:27 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    TimelineComponent,
+    FooterComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
