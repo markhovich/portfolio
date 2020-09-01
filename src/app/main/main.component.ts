7dbdd4002b3298f1b524b59e7367976042674969
julienm <marque_julien@hotmail.fr>
2020-09-01 16:36:38 +0200

    project details updated

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  currentIndex: number;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClickDisplay(index: number){
+    this.currentIndex = index;
+
+    const block = document.getElementsByClassName('project-details')[index];
+    const curtain = document.getElementById('page');
+
+    if(!block.classList.contains('displayed')){
+      block.classList.add('displayed');
+      curtain.classList.add('curtain');
+    }
+  }
+
+  removeCurtain(){
+    document.getElementsByClassName('project-details')[this.currentIndex].classList.remove('displayed');
+    document.getElementById('page').classList.remove('curtain');
   }
 
 }
